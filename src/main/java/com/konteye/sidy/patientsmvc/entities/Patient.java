9158a489050bbf62c12274892dd64b29f661730f
SKonteye <skonteye2@gmail.com>
2023-07-30 23:33:10 +0000

    Complete CRUD implementation

--- src/main/java/com/konteye/sidy/patientsmvc/entities/Patient.java
+++ src/main/java/com/konteye/sidy/patientsmvc/entities/Patient.java
@@ -1,9 +1,13 @@
 package com.konteye.sidy.patientsmvc.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -14,9 +18,13 @@ public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 4, max = 40)
     private String name;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 }
--- src/main/java/com/konteye/sidy/patientsmvc/web/PatientController.java
+++ src/main/java/com/konteye/sidy/patientsmvc/web/PatientController.java
@@ -2,12 +2,15 @@ package com.konteye.sidy.patientsmvc.web;
 
 import com.konteye.sidy.patientsmvc.entities.Patient;
 import com.konteye.sidy.patientsmvc.repositories.PatientRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -45,6 +48,28 @@ public class PatientController {
     public List<Patient> patientsList(){
         List<Patient> patientEndpoint = patientRepository.findAll();
         return patientEndpoint ;
+    }
+    @GetMapping("/formPatients")
+    public String formpatient (Model model){
+        model.addAttribute("patient",new Patient());
+        return "formPatients";
+    }
+    @PostMapping("/save")
+    public String save(Model model, @Valid Patient patient,BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()) return "formPatients";
+        patientRepository.save(patient);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/editPatient")
+    public String editpatient (Model model, Long id, int page, String keyword){
+        Patient patient = patientRepository.findById(id).orElse(null);
+        if (patient==null) throw new RuntimeException("Patient Not found");
+        model.addAttribute("patient",patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatients";
     }
 
 }
